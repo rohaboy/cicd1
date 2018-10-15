27042cb3078fca03ae8f1b81b5e7584e0b32a7b9
Jerry Mouse <JerryMouse@Test.com>
2018-10-15 09:53:56 -0400

    Initial Commit - Source Code

---
+++ CICDDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CICDDemo.Startup))]
+namespace CICDDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
